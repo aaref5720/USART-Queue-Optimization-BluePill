6b150167fbde4c646e45b39accecbe10a208ac57
Abdelrahman Aref <abdo01445@gmail.com>
2024-04-13 18:44:03 +0200

    APP Layer

---
+++ APP/USART_Debugging_Using_Interrupt.h
@@ -0,0 +1,23 @@
+/*
+ * USART_Debugging_Using_Interrupt.h
+ *
+ *  Created on: Apr 1, 2023
+ *      Author: Abdelrahman Aref
+ */
+
+#ifndef USART_DEBUGGING_USING_INTERRUPT_H_
+#define USART_DEBUGGING_USING_INTERRUPT_H_
+
+/*******************************************************************************
+                                     Includes
+*******************************************************************************/
+#include "USART.h"
+
+/*******************************************************************************
+                                  APP Function
+*******************************************************************************/
+void USART1_CallBack(void);
+void USART_Debugging_Using_Interrupt(void);
+
+
+#endif /* USART_DEBUGGING_USING_INTERRUPT_H_ */
